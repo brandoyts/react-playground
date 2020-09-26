ac9a4cac2cb465df68e66ab59c3a7e5c75d2411c
brandoyts <brando.endona.dev@gmail.com>
2020-09-27 01:20:22 +0800

    unmount cleanup

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -4,15 +4,27 @@ import axios from "axios";
 const useFetchData = (pathName) => {
   const [data, setData] = useState([]);
 
-  const fetchData = async (pathName) => {
-    const url = `http://jsonplaceholder.typicode.com${pathName}`;
-    const { data } = await axios.get(url);
-    setData(data);
-  };
-
   useEffect(() => {
+    const source = axios.CancelToken.source();
+
+    const fetchData = (pathName) => {
+      const url = `http://jsonplaceholder.typicode.com${pathName}`;
+      axios
+        .get(url, { cancelToken: source.token })
+        .then((response) => {
+          setData(response.data);
+        })
+        .catch((err) => {
+          console.log("Catched error: " + err.message);
+        });
+    };
+
     fetchData(pathName);
-  }, []);
+
+    return () => {
+      source.cancel("cancel request");
+    };
+  }, [pathName]);
 
   return [data];
 };
